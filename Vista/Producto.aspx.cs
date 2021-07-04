b69db67053893d574b9fceb9f79703e4aac77b24
Alexander25470 <61051951+Alexander25470@users.noreply.github.com>
2021-07-04 13:37:56 -0300

    problema al comprar producto con precio decimal

--- Vista/Producto.aspx.cs
+++ Vista/Producto.aspx.cs
@@ -150,7 +150,7 @@ namespace Vista
                 lbl_Agregado.Text = "Has agregado " + car._articulos[posInCarrito].CantAgregar.ToString() + " " + car._articulos[posInCarrito].Producto.Nombre + " al carrito";
                 car._articulos[posInCarrito].CantAgregar =0;
                 lbl_Cantidad.Text = car._articulos[posInCarrito].CantAgregar.ToString();
-                lbl_SubTotal.Text = Convert.ToString(car._articulos[posInCarrito].Cant * Convert.ToInt32(lbl_precio.Text));
+                lbl_SubTotal.Text = Convert.ToString(car._articulos[posInCarrito].Cant * Convert.ToDouble(lbl_precio.Text));
                 lblCantidadActual.Text = car._articulos[posInCarrito].Cant.ToString();
             }
             else
